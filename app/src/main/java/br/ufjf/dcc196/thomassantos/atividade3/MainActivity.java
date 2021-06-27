115e8640c64da05d74eb632b36c127143fc2901a
Thomas Santos <thomasbsjf@gmail.com>
2021-06-27 18:47:28 -0300

    Funções calcular e limpar

--- app/src/main/java/br/ufjf/dcc196/thomassantos/atividade3/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/thomassantos/atividade3/MainActivity.java
@@ -3,14 +3,54 @@ package br.ufjf.dcc196.thomassantos.atividade3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewResult;
+    private EditText editTextNumero1;
+    private EditText editTextNumero2;
+    private RadioGroup radioGroupOperacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textViewResult = findViewById(R.id.textViewResult);
+        editTextNumero1 = findViewById(R.id.editTextNumero1);
+        editTextNumero2 = findViewById(R.id.editTextNumero2);
+        radioGroupOperacao = findViewById(R.id.radioGroupOperacao);
 
+    }
+
+    public void calcular(View view){
+        Double resultado = 0.0;
+        Double numero1 = 0.0;
+        Double numero2 = 0.0;
+
+        switch(radioGroupOperacao.getCheckedRadioButtonId()){
+            case R.id.radioButtonSoma:
+                resultado = numero1 + numero2;
+                break;
+            case R.id.radioButtonSub:
+                resultado = numero1 - numero2;
+                break;
+            case R.id.radioButtonMult:
+                resultado = numero1 * numero2;
+                break;
+            default:
+                resultado = numero1 / numero2;
+        }
+
+        textViewResult.setText(resultado.toString());
+    }
+
+    public void limpar(View view){
+        editTextNumero1.setText("");
+        editTextNumero2.setText("");
+        textViewResult.setText("");
     }
 }
